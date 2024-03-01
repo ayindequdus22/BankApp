98e0989e26b94621b077205c0f46f404d2266af8
ayindequdus22 <ayindeabdulqudus22@gmail.com>
2024-03-01 12:13:10 -0800

    sign up page updated

--- src/gui/SignUpPage.java
+++ src/gui/SignUpPage.java
@@ -6,24 +6,31 @@ import java.awt.event.*;
 
 public class SignUpPage extends JFrame implements ActionListener {
 
-    private JTextField usernameField;
+    private JTextField usernameField,firstNameField,secondNameField;
     private JPasswordField passwordField;
     private JButton SignupButton, loginButton;
-    private JLabel usernameLabel, passwordLabel;
+    private JLabel firstNameLabel,secondNameLabel,usernameLabel, passwordLabel;
     private JPanel panel;
 
     public SignUpPage() {
         setTitle("Signup");
-        setSize(300, 200);
+        setSize(300, 250);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocation(500,250);
         setResizable(false);
         panel = new JPanel();
-        panel.setLayout(new GridLayout(3, 2));
+        panel.setLayout(new GridLayout(5, 2));
 
+        firstNameLabel = new JLabel("First Name:");
+        secondNameLabel = new JLabel("Second Name:");
+        usernameLabel = new JLabel("Username:");
+        passwordLabel = new JLabel("Password:");
         usernameLabel = new JLabel("Username:");
         passwordLabel = new JLabel("Password:");
 
+
+        firstNameField = new JTextField();
+        secondNameField = new JPasswordField();
         usernameField = new JTextField();
         passwordField = new JPasswordField();
 
@@ -33,6 +40,11 @@ public class SignUpPage extends JFrame implements ActionListener {
         SignupButton.addActionListener(this);
         loginButton.addActionListener(this);
 
+
+        panel.add(firstNameLabel);
+        panel.add(firstNameField);
+        panel.add(secondNameLabel);
+        panel.add(secondNameField);
         panel.add(usernameLabel);
         panel.add(usernameField);
         panel.add(passwordLabel);
@@ -61,6 +73,8 @@ public class SignUpPage extends JFrame implements ActionListener {
     private boolean validateSignup(String username, String password) {
         return username.equals("admin") && password.equals("admin123");
     }
-
+public static void main(String[] args) {
+    new SignUpPage();
+}
   
 }
